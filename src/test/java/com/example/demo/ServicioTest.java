49abdcb7230240e7d7779989d9591a6e0a940af1
Santiago Barrera <santiago.barrerae@gmail.com>
2022-03-21 20:16:04 -0500

    test para buscarUno buscarTodos buscarTodosLento

---
+++ src/test/java/com/example/demo/ServicioTest.java
@@ -0,0 +1,42 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ServicioTest {
+    @Autowired
+    Servicio servicio;
+    @Test
+    void testMono() {
+        Mono<String> uno = servicio.buscarUno();
+        StepVerifier.create(uno).expectNext("Pedro").verifyComplete();
+    }
+    @Test
+    void testVarios() {
+        Flux<String> uno = servicio.buscarTodos();
+        StepVerifier.create(uno).expectNext("Pedro").expectNext("Maria").expectNext("Jesus").expectNext("Carmen").verifyComplete();
+    }
+
+    @Test
+    void testVariosLento() {
+        Flux<String> uno = servicio.buscarTodosLento();
+        StepVerifier.create(uno)
+                .expectNext("Pedro")
+                .thenAwait(Duration.ofSeconds(1))
+                .expectNext("Maria")
+                .thenAwait(Duration.ofSeconds(1))
+                .expectNext("Jesus")
+                .thenAwait(Duration.ofSeconds(1))
+                .expectNext("Carmen")
+                .thenAwait(Duration.ofSeconds(1)).verifyComplete();
+    }
+}
